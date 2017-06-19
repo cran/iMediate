5395cc79f3a72a3d8426730fa9dba63a8cc3f78c
Kai Wang <kai-wang@uiowa.edu>
2017-06-19 22:26:31 +0000

    version 0.3

--- R/mdn.R
+++ R/mdn.R
@@ -1,33 +1,64 @@
-mdn = function(fit.M, fit.Y, X, test="S test", sig.level=0.05) {
+mdn = function(fit.M, fit.Y, X, test="LR", sig.level=0.05, B=100) {
     M = all.vars(formula(fit.M))[1]
-    a0 = update(fit.M, as.formula(paste(".~. - ", X)))                   # no X effect
-    b0 = update(fit.Y, as.formula(paste(".~. - ", M)))                   # no M effect
-    b0c0 = update(fit.Y, as.formula(paste(".~. - ", M, "-", X)))         # no M and X effect
+    nn = length(resid(fit.M))
+        
+    mdn2 = function(fit.M, fit.Y, X, boot=TRUE){
+    	    if(boot) idx = sample(1:nn, replace=TRUE) else idx=1:nn
+    	    tmp.M = update(fit.M, data=fit.M$model[idx,])
+	    tmp.Y = update(fit.Y, data=fit.Y$model[idx,])
+        a0 = update(tmp.M, as.formula(paste(".~. - ", X)))                   # no X effect
+        b0 = update(tmp.Y, as.formula(paste(".~. - ", M)))                   # no M effect
+        b0c0 = update(tmp.Y, as.formula(paste(".~. - ", M, "-", X)))         # no M and X effect
 
-    l_a = 2*(logLik(fit.M) - logLik(a0))
-    l_b.c = 2*(logLik(fit.Y) - logLik(b0))
-    l_c = 2*(logLik(b0) - logLik(b0c0)) 
-    mediated = min(l_a, l_b.c)
-    mediator = l_a + l_b.c
-    total = l_a+l_b.c+l_c
+        l_a = 2*(logLik(tmp.M) - logLik(a0))    
+        l_b.c = 2*(logLik(tmp.Y) - logLik(b0))
+        l_c = 2*(logLik(b0) - logLik(b0c0)) 
+        mediated = min(l_a, l_b.c)
+        mediator = l_a + l_b.c
+        total = l_a+l_b.c+l_c
+        
+        c(l_a, l_b.c, max(l_a, l_b.c), mediated, mediator, l_c, total, mediated/mediator, mediated/total)
+    }
+    
+    obvd = mdn2(fit.M, fit.Y, X, boot=FALSE)
+    l_a = obvd[1]
+    l_b.c = obvd[2]
+    mediator = obvd[5]
+    predictor = obvd[6]
+    total = obvd[7]
 
-    if(test == "S test"){
+    if(test == "S"){
     	    ra = coef(summary(fit.M))[X, "Estimate"]
 	    rb = coef(summary(fit.Y))[M, "Estimate"]
 	    Test =  S.test(1-pnorm(sign(ra)*sqrt(l_a)), 1-pnorm(sign(rb)*sqrt(l_b.c)), sig.level) 
     }
-    if(test == "LRT"){
+    if(test == "LR"){
         c.value = qchisq(1 - sig.level, 1)
 	    Test = (l_a >= c.value) & (l_b.c >= c.value)
     }
-    if(test != "S test" & test != "LRT") stop("Unknown test name")
+    if(test != "S" & test != "LR") stop("Unknown test name")
 
-    result = data.frame(Estimate = c(max(l_a, l_b.c), mediated, mediator, l_c, total,   
-                            mediated/mediator, mediated/total),
-             P = c(NA, 1-Test, 1-pchisq(mediator, 2), 1-pchisq(l_c, 1), 1-pchisq(total, 3), NA, NA))
+    if (B>0){
+    	    BS = matrix(0, nrow=B, ncol=7)
+    	    for (i in 1:B) BS[i,] = mdn2(fit.M, fit.Y, X)[-(1:2)]
+        BS[,1:5] = BS[,1:5]/nn
+        lower = apply(BS, 2, quantile, probs = sig.level/2)
+        upper = apply(BS, 2, quantile, probs = 1-sig.level/2)
+
+        result = data.frame(Estimate = c(obvd[3:7]/nn, obvd[8:9]),
+                            Lower = lower,
+                            Upper = upper,
+                            Stat = c(obvd[3:7], NA, NA),
+                            P = c(NA, 1-Test, 1-pchisq(mediator, 2), 1-pchisq(predictor, 1), 1-pchisq(total, 3), NA, NA))
+    }
+    else {
+        result = data.frame(Estimate = c(obvd[3:7]/nn, obvd[8:9]),
+                            Stat = c(obvd[3:7], NA, NA),
+                            P = c(NA, 1-Test, 1-pchisq(mediator, 2), 1-pchisq(predictor, 1), 1-pchisq(total, 3), NA, NA))
+    }
     dimnames(result)[[1]] = c("  Unmediated", "  Mediated", "Mediator", "Predictor", 
-                              "Overall", "Mediated/Predictor", "Mediated/Overall")
+                              "Total", "Mediated/Mediator", "Mediated/Total")
     
-    structure(list(result=result, test=test, sig.level=sig.level), class = "mdn")
+    structure(list(result=result, test=test, sig.level=sig.level, sample.size=nn, B=B), class = "mdn")
 }
 
--- R/print.mdn.R
+++ R/print.mdn.R
@@ -3,11 +3,23 @@ print.mdn = function (x, ...)
     cat("\nMediation Analysis via Likelihood \n\n")
     tmp = x$result
     tmp$Estimate = round(tmp$Estimate, 4)
+    if (x$B>0){
+    	tmp$Lower = round(tmp$Lower, 4)
+        tmp$Upper = round(tmp$Upper, 4)
+    }
+    tmp$Stat = round(tmp$Stat, 4)
+    tmp$Stat[is.na(tmp$Stat)] = " "
     tmp$P = signif(tmp$P, 4)
     tmp$P[is.na(tmp$P)] = " "
     if(tmp$P[2]=="1") tmp$P[2] = paste(">", x$sig.level)
     if(tmp$P[2]=="0") tmp$P[2] = paste("<", x$sig.level)
+    if (x$B>0){
+    	    cf = paste(100*(1-x$sig.level), "%", sep="")
+        names(tmp)[2:3] = c(paste("LB", cf, "CI"), paste("UB", cf, "CI"))
+    }
     print(tmp)
-    cat("\nSignificance of the mediated effect is determined by", x$test)
-    cat("\nThe range of Mediated/Mediator or Mediated/Overall is [0, 0.5]")
+    cat(paste("\n", x$sample.size, " subjects", sep=""))
+    if (x$B>0) cat(paste("\n", x$B, " bootstrap samples for confidence intervals", sep=""))
+    cat("\nSignificance of the mediated effect is determined by", x$test, "test")
+    cat("\nUpper bound for Mediated/Mediator or Mediated/Total is 0.5\n\n")
 }
